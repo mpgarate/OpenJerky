f8d80127486beb9d7eb333370ced80d34c02a3ab
mpgarate <mpgarate@gmail.com>
2013-08-01 22:27:13 -0400

    Update recipe.rb

--- recipe.rb
+++ recipe.rb
@@ -2,13 +2,13 @@ class Jerky
 	def ingredients
 		@ingredients = {
 			"beef (brisket cut)" 	=> [8, "lbs"],
-			"habanero peppers" 		=> [16, "count"],
-			"soy sauce" 					=> [4, "cups"],
-			"sriracha sauce"	 		=> [0.5, "cups"],
-			"liquid smoke" 				=> [1, "tbsp"],
+			"habanero peppers" 	=> [16, "count"],
+			"soy sauce" 		=> [4, "cups"],
+			"sriracha sauce"	=> [0.5, "cups"],
+			"liquid smoke" 		=> [1, "tbsp"],
 			"crushed red peppers"	=> [1, "eyeball (a lot)"],
-			"onion powder" 				=> [1, "eyeball (enough)"],
-			"garlic salt" 				=> [1, "eyeball (enough)"]
+			"onion powder" 		=> [1, "eyeball (enough)"],
+			"garlic salt" 		=> [1, "eyeball (enough)"]
 		}
 	end
 end
