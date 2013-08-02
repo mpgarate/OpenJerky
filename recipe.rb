d39d4bed2101a05c29d06019e8f52cb382802bac
Michael Garate <mpgarate@gmail.com>
2013-08-01 22:24:33 -0400

    stored amounts and measurement types as arrays in the ingredients hash

--- recipe.rb
+++ recipe.rb
@@ -1,14 +1,14 @@
 class Jerky
 	def ingredients
 		@ingredients = {
-			"beef (brisket cut)" => "8 lbs",
-			"habanero peppers" => "16 count",
-			"soy sauce" => "4 cups",
-			"sriracha sauce" => "1/2 cup",
-			"liquid smoke" => "1 tbsp",
-			"crushed red peppers" => "eyeball (a lot)",
-			"onion powder" => "eyeball (enough)",
-			"garlic salt" => "eyeball (enough)"
+			"beef (brisket cut)" 	=> [8, "lbs"],
+			"habanero peppers" 		=> [16, "count"],
+			"soy sauce" 					=> [4, "cups"],
+			"sriracha sauce"	 		=> [0.5, "cups"],
+			"liquid smoke" 				=> [1, "tbsp"],
+			"crushed red peppers"	=> [1, "eyeball (a lot)"],
+			"onion powder" 				=> [1, "eyeball (enough)"],
+			"garlic salt" 				=> [1, "eyeball (enough)"]
 		}
 	end
 end
